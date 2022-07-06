7495a09880f1879d02195777b3289f57bc1d5ab6
swanhtethtetswan <ucancallmex@gmail.com>
2022-07-06 14:41:32 -0400

    set up react routers and main containers

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const WebpackDevServer = require('webpack-dev-server');
+// const WebpackDevServer = require('webpack-dev-server');
 
 
 const config = {
