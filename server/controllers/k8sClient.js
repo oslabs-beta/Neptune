bc17bf8b0bb8225dfda16769398920c5dede1d0c
Jin Yoo <iyoojin@gmail.com>
2022-07-07 21:07:09 -0400

    k8clientcontrollers and routes added

---
+++ server/controllers/k8sClient.js
@@ -0,0 +1,113 @@
+// k8s DASHBOARD
+// import express from "express";
+// K8s API
+// convert controllers to promise chaining
+const k8s = require('@kubernetes/client-node');
+
+//New instance of k8s, (Kube config are the files that connect )
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+const k8sApiSvc = kc.makeApiClient(k8s.CoreV1Api); // Queries services
+const k8sApiDepl = kc.makeApiClient(k8s.AppsV1Api); // Queries deployments
+//const metricsClient = new k8s.Metrics(kc);
+
+k8sController = {};
+
+// example from official github repo
+k8sApiSvc.listNamespacedPod('default').then((res) => {
+  console.log(res.body);
+});
+
+// Getting all pods
+k8sController.getAllPods = async (req, res, next) => {
+  try {
+    const podsResult = await k8sApiSvc.listPodForAllNamespaces();
+    res.locals.podList = podsResult;
+
+    return next();
+  } catch (err) {
+    return next({
+      log: 'Error getting data from getAllPods',
+      status: 404,
+      message: {
+        err: 'An error happened trying to get the data from getAllPods',
+      },
+    });
+  }
+};
+
+// Getting list of nodes and list of component statuses
+k8sController.getAllNodes = async (req, res, next) => {
+  const { name } = req.params;
+  try {
+    const nodeResult = await k8sApiSvc.listNode(name);
+    res.locals.nodeList = nodeResult.body;
+
+    const nodeStatus = await k8sApiSvc.listComponentStatus();
+    res.locals.nodeList.nodeStatus = nodeStatus.body;
+
+    return next();
+  } catch (err) {
+    return next({
+      log: 'Error getting data from getAllNodes',
+      status: 404,
+      message: {
+        err: 'An error happened trying to get All Nodes',
+      },
+    });
+  }
+};
+
+// Getting all namespaces
+k8sController.getAllNamespaces = async (req, res, next) => {
+  try {
+    const namespaceResult = await k8sApiSvc.listNamespace();
+    res.locals.namespace = namespaceResult;
+    return next();
+  } catch (err) {
+    return next({
+      log: 'Error getting data from getAllNamespaces',
+      status: 404,
+      message: {
+        err: 'An error happened trying to get the data from getAllNamespaces',
+      },
+    });
+  }
+};
+
+// Getting all deployments in cluster
+k8sController.getDeployment = async (req, res, next) => {
+  try {
+    const getDeployment = await k8sApiDepl.listDeploymentForAllNamespaces();
+    res.locals.deployment = getDeployment.body;
+    return next();
+  } catch (err) {
+    return next({
+      log: 'Error getting data from getDeploymentList',
+      status: 404,
+      message: {
+        err: 'An error happened trying to get the data from getDeploymentList',
+      },
+    });
+  }
+};
+
+// Getting all services in cluster
+k8sController.getService = async (req, res, next) => {
+  try {
+    const getService = await k8sApiSvc.listServiceForAllNamespaces();
+    res.locals.service = getService.body;
+    return next();
+  } catch (err) {
+    return next({
+      log: 'Error getting data from getDeploymentList',
+      status: 404,
+      message: {
+        err: 'An error happened trying to get the data from getDeploymentList',
+      },
+    });
+  }
+};
+
+module.exports = k8sController;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,20 +1,32 @@
-import express from 'express';
-import k8sController from '../controllers/controllers'
-
-
+const express = require('express');
 const k8sRouter = express.Router();
 
-// GET 
-k8sRouter.get('/node/:name',k8sController.getAllNodes , (req, res) => {
-  res.status(200).json(res.locals.nodeList);
-});
+const k8sController = require('../controllers/k8sClient');
 
-k8sRouter.get('/pod/:name',k8sController.getAllPods, (req, res) => {
+// /api/k8s/"NEW END POINT HERE"
+// ROUTE FOR PODS
+k8sRouter.get('/pod', k8sController.getAllPods, (req, res) => {
   res.status(200).json(res.locals.podList);
 });
 
-k8sRouter.get('/namespace/:name',k8sController.getAllNamespaces , (req, res) => {
+// ROUTE FOR NODE LIST AND NODE STATUS
+k8sRouter.get('/node', k8sController.getAllNodes, (req, res) => {
+  res.status(200).json(res.locals.nodeList);
+});
+
+// ROUTE FOR NAMESPACES
+k8sRouter.get('/namespace', k8sController.getAllNamespaces, (req, res) => {
   res.status(200).json(res.locals.namespace);
+});
+
+// ROUTE FOR DEPLOYMENTS
+k8sRouter.get('/deployment', k8sController.getDeployment, (req, res) => {
+  res.status(200).json(res.locals.deployment);
+});
+
+// ROUTE FOR SERVICES
+k8sRouter.get('/services', k8sController.getService, (req, res) => {
+  res.status(200).json(res.locals.service);
 });
 
 module.exports = k8sRouter;
